d4520af0f12c1ef2574de5d10494bfb372d1ebc6
sarbia-offical <xm36447864@126.com>
2021-12-06 23:59:40 +0800

    完善歌曲播放页面

--- src/components/player/index.vue
+++ src/components/player/index.vue
@@ -19,13 +19,47 @@
         </div>
         <div class="circleBtn">
           <iconComponent
-            :iconPath="'icon-gequpaihang_fill'"
+            :iconPath="'icon-xianhua'"
             :iconColor="'#000000'"
           ></iconComponent>
         </div>
       </div>
       <div :style="artistStyle">
-        <!-- {{ singer?.artist?.cover }} -->
+        <!-- <div class="lyric"></div> -->
+      </div>
+      <div class="detail">
+        <div class="songName">{{ currentSongs.name }}</div>
+        <div class="singers">
+          <div 
+            v-for="(item, index) in currentSongs.ar" 
+            :key="index"
+            class="singer">
+            {{ item.name }} 
+          </div>
+        </div>
+      </div>
+      <div class="tools">
+        <div class="circleBtn toolsBtn">
+          <iconComponent
+            :iconPath="'icon-shangyishou_huaban'"
+            :iconColor="'#000000'"
+            :fontSize="'30px'"
+          ></iconComponent>
+        </div>
+        <div class="circleBtn toolsBtn">
+          <iconComponent
+            :iconPath="'icon-kaishi_fill'"
+            :iconColor="'#000000'"
+            :fontSize="'50px'"
+          ></iconComponent>
+        </div>
+        <div class="circleBtn toolsBtn">
+          <iconComponent
+            :iconPath="'icon-shangyishou_huaban-copy'"
+            :iconColor="'#000000'"
+            :fontSize="'30px'"
+          ></iconComponent>
+        </div>
       </div>
     </div>
   </div>
@@ -47,15 +81,15 @@ export default defineComponent({
       console.log(store.state);
       const singer = store.state.singer;
       return {
-        marginTop: '.3rem',
-        height: '70vh',
-        width: '100%',
-        backgroundImage: `url(${singer.artist.cover}?param=375y375)`,
-        backgroundSize: 'cover'
-      }
-    })
+        position: "relative",
+        marginTop: ".3rem",
+        height: "50vh",
+        width: "100%",
+        backgroundImage: `url(${singer?.artist?.cover}?param=375y375)`,
+        backgroundSize: "cover",
+      };
+    });
     watch(currentSongs, async (newVal, oldVal) => {
-      console.log(currentSongs);
       const res = await Player.getSongUrl(newVal.id);
       const audioEle = audioRef.value;
       audioEle.src = res.data[0].url;
@@ -65,12 +99,13 @@ export default defineComponent({
       store.commit("setFullScreen", false);
     };
     return {
+      store,
       currentSongs,
       fullScreen,
       singer,
       audioRef,
       small,
-      artistStyle
+      artistStyle,
     };
   },
 });
@@ -86,26 +121,74 @@ export default defineComponent({
   background-color: #fdfbfb;
   z-index: 99999;
   padding: 0.25rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  .circleBtn {
+    width: 0.5rem;
+    height: 0.5rem;
+    background-color: #fbf6f6;
+    padding: 0.1rem;
+    @include aic_jcc();
+    box-shadow: 3px 4px 4px 0px #e5d9d9;
+    border-radius: 50%;
+  }
   .topContent {
     width: 100%;
     height: 0.625rem;
     @include aic_jcsb();
-    .circleBtn {
-      width: 0.5rem;
-      height: 0.5rem;
-      background-color: #fbf6f6;
-      padding: 0.1rem;
-      @include aic_jcc();
-      box-shadow: 3px 4px 4px 0px #e5d9d9;
-      border-radius: 50%;
-    }
   }
-  .bodyContent{
-    margin-top: .3rem;
+  .bodyContent {
+    margin-top: 0.3rem;
     height: 70vh;
     width: 100%;
-    background-image: url('http://p1.music.126.net/LCWqYYKoCEZKuAC3S3lIeg==/109951165034938865.jpg?param=375y375');
+    background-image: url("http://p1.music.126.net/LCWqYYKoCEZKuAC3S3lIeg==/109951165034938865.jpg?param=375y375");
     background-size: cover;
+  }
+  .lyric {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 90vw;
+    height: 20vh;
+    background-color: #242326;
+    border-top-left-radius: 0.7rem;
+    border-bottom-left-radius: 0.7rem;
+  }
+  .detail{
+    flex: .6;
+    width: 90vw;
+    margin: 0 auto;
+    margin-top: 1vh;
+    @include aic_jcc();
+    flex-direction: column;
+    .songName{
+      font-family: hyxhkj;
+      font-size: 0.575rem;
+    }
+    .singers{
+      margin-top: .3rem;
+      color: #888888;
+      @include aic_jcc();
+      font-size: .3rem;
+      div ~ div{
+        margin-left: .2rem;
+      }
+    }
+  }
+  .tools {
+    width: 100%;
+    @include aic_jcsb();
+    justify-content: space-evenly;
+    .toolsBtn{
+      width: auto;
+      height: auto;
+      transition: box-shadow .2s ease-out;
+    }
+    .toolsBtn:active{
+      @include shadow2-2();
+    }
   }
 }
 </style>
